87a332cc1d17c9ead980024577d3f38e486b51c9
Carl Sobolewski <sobolews@ualberta.ca>
2014-10-08 16:17:54 -0600

    Deleted and restarted new project: Changed com.example.agora to com.brogrammers.agora

--- Agora/gen/com/brogrammers/agora/R.java
+++ Agora/gen/com/brogrammers/agora/R.java
@@ -5,7 +5,7 @@
  * should not be modified by hand.
  */
 
-package com.example.agora;
+package com.brogrammers.agora;
 
 public final class R {
     public static final class attr {
--- Agora/src/com/brogrammers/agora/MainActivity.java
+++ Agora/src/com/brogrammers/agora/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.agora;
+package com.brogrammers.agora;
 
 import android.app.Activity;
 import android.os.Bundle;
