88d63545d2abcb862ef897333a1fd9b8d3b5f636
jcairo <joncairo@gmail.com>
2014-10-15 22:27:04 -0600

    Added test for counting the number of answers a question has

--- AgoraTest/src/com/brogrammers/agora/test/AddQuestionTest.java
+++ AgoraTest/src/com/brogrammers/agora/test/AddQuestionTest.java
@@ -2,22 +2,37 @@ package com.brogrammers.agora.test;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 
 import junit.framework.TestCase;
 
 public class AddQuestionTest extends TestCase {
 	QuestionController controller = QuestionController.getController();
-	String questionTitle = String.valueOf(System.currentTimeMillis()) + ' Test';
+	String questionTitle = String.valueOf(System.currentTimeMillis());
 	
-	BufferedImage image = null; 
+	// http://stackoverflow.com/questions/5882005/how-to-download-image-from-any-web-page-in-java
+	// Author: planetjones
+	// October 15, 2014
+	Image img = null; 
 	try {
-	    img = ImageIO.read(new File("strawberry.jpg"));
+	    URL url = new URL("http://www.4pipetools.com/external/commerce/1/gfx/thumb/MON1372.jpg");
+		img = ImageIO.read(url);
 	} catch (IOException e) {
 	}
 	
-	controller.addQuestion(questionTitle, 'Test Body', image);
+	controller.addQuestion(questionTitle, 'Body Test', img);
 	
 	WebServiceModel webServiceModel = WebServiceModel.getModel();
 	Question addedQuestion = webServiceModel.searchQuestions('questionTitle');
 	assertTrue(addedQuestion.size() == 1);
+	assertTrue(addedQuestion.get(0).title == questionTitle);
+	assertTrue(addedQuestion.get(0).body == 'Body Test');
+	assertTrue(addedQuestion.get(0).image != null);
+	
+	LocalCacheModel localCacheModel = LocalCacheModel.getModel();
+	Question addedQuestion = localCacheModel.searchQuestions('questionTitle');
+	assertTrue(addedQuestion.size() == 1);
+	assertTrue(addedQuestion.get(0).title == questionTitle);
+	assertTrue(addedQuestion.get(0).body == 'Body Test');
+	assertTrue(addedQuestion.get(0).image != null);
 }
