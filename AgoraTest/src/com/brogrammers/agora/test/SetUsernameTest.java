f0e581db4981a1ac48a34851079f230c68f716f6
Carl Sobolewski <sobolews@ualberta.ca>
2014-10-15 20:45:56 -0600

    Added one username test

--- AgoraTest/src/com/brogrammers/agora/test/SetUsernameTest.java
+++ AgoraTest/src/com/brogrammers/agora/test/SetUsernameTest.java
@@ -1,7 +1,13 @@
 package com.brogrammers.agora.test;
 
 import junit.framework.TestCase;
+import com.brogrammers.agora.DeviceUser
 
 public class SetUsernameTest extends TestCase {
-
+	DeviceUser testUser;
+	void testUsername() {
+		testUser = new DeviceUser("Brogram");
+		assertTrue(testUser.getUsername().equals("Brogram"));
+	}
+	
 }
