40a3ea607baffc89aa0d30f55e5a4b7303c7ef51
jcairo <joncairo@gmail.com>
2014-10-16 22:05:08 -0600

    Added test to ensure 2 answered questions contain only answers to themselves and not each other.

--- AgoraTest/src/com/brogrammers/agora/test/FindNumberOFQuestionAnswersTest.java
+++ AgoraTest/src/com/brogrammers/agora/test/FindNumberOFQuestionAnswersTest.java
@@ -19,23 +19,47 @@ public class FindNumberOFQuestionAnswersTest extends TestCase{
 	} catch (IOException e) {
 	}
 	
-	controller.addQuestion(questionTitle, 'Body Test', img);
+	long questionID = controller.addQuestion(questionTitle, 'Body Test', img);
 	
 	WebServiceModel webServiceModel = WebServiceModel.getModel();
 	LocalCacheModel localCacheModel = LocalCacheModel.getModel();
-	Question addedQuestion = webServiceModel.searchQuestions('questionTitle').get(0);
+	Question addedQuestion = webServiceModel.searchQuestions(questionTitle).get(0);
 	Question addedQuestionCached = localCacheModel.getQuestions().get(0);
 	assertTrue(addedQuestion.countAnswers() == 0);
 	assertTrue(addedQuestionCached.countAnswers() == 0);
 
-	long questionID = addedQuestion.uniqueID;
-	webServiceModel.addAnswer('Body Test', questionID, img);
-	webServiceModel.addAnswer('Body Test', questionID, img);
-	webServiceModel.addAnswer('Body Test', questionID, img);
+	long answerId1 = webServiceModel.addAnswer('Body Test', questionID, img);
+	long answerId2 = webServiceModel.addAnswer('Body Test', questionID, img);
+	long answerId3 = webServiceModel.addAnswer('Body Test', questionID, img);
 
 	assertTrue(addedQuestion.countAnswer() == 3);
 	assertTrue(addedQuestion.image != null);
 	assertTrue(addedQuestionCached.countAnswers() == 3);
 	assertTrue(addedQuestionCached.image != null);
+	
+	// add another question with answers and ensure each question
+	// only maintains references to its answers and not the other questions.
+	String questionTitle2 = String.valueOf(System.currentTimeMillis());
+	long questionID2 = controller.addQuestion(questionTitle2, 'Body Test', img);
+	Question addedQuestion2 = webServiceModel.searchQuestions(questionTitle2).get(0);
+	
+	// assumes the model returns the answer id
+	ArrayList<long> answerIds = new ArrayList<long>;
+	answerIds.add(webServiceModel.addAnswer('Body Test', questionID2, img));	
+	answerIds.add(webServiceModel.addAnswer('Body Test', questionID2, img));
+	answerIds.add(webServiceModel.addAnswer('Body Test', questionID2, img));
+	
+	// ensure the first question created does not have reference to the new answers.
+	Question addedQuestion = webServiceModel.searchQuestions(questionTitle).get(0);
+	for (Answer a: addedQuestion.answers){
+		assertFalse(answerIds.contains(a.uniqueID));
+	}
+
+	
+	
+	
+
+	
+	
 
 }
