15258bfa40b9793fa2ab45b4299faed17b242d5d
Kevin Tang <tkevin@ualberta.ca>
2014-10-17 03:05:08 -0600

    Modified dia to include GetAnswerById. Added Sort tests.

---
+++ AgoraTest/src/com/brogrammers/agora/test/SortByDateTest.java
@@ -0,0 +1,40 @@
+package com.brogrammers.agora.test;
+
+import java.util.Date;
+
+import junit.framework.TestCase;
+
+
+public class SortByDateTest extends TestCase {
+	QuestionController controller = QuestionController.getController();
+	WebServiceModel webModel = WebServiceModel.getModel();
+	List<Question> questions = weModel.getQuestions();
+	
+	Question firstDate= new Question("Test post please ignore", "ignore pls", null );
+	Question secondDate = new Question("Why can't my Meowth talk?", "talk pls", null );
+	Question thirdDate = new Question("Where is infinity and beyond?", "the claw", null );
+	Question fourthDate = new Question("There's a snake in my boot.", "howdy howdy howdy", null );
+	
+	//Order should be {fourthDate, secondDate, thirdDate, firstDate}
+	//because automatically, newest is first
+	long firstID = controller.addQuestion(noVote);
+	long thirdID= controller.addQuestion(threeVote);
+	long secondID = controller.addQuestion(fiveVote);
+	long fourthID = controller.addQuestion(tenVote);
+	
+	//manually set date so fourth is oldest and first is now newest
+	controller.getQuestionById(fourthID).setDate(Date());
+	controller.getQuestionById(thirdID).setDate(Date());
+	controller.getQuestionById(secondID).setDate(Date());
+	controller.getQuestionById(firstID).setDate(Date());
+	
+	//assuming enum {VOTES, DATE};
+	questions.setFilter(1);
+	
+	//order should now be { firstDate, secondDate, thirdDate, fourthDate}
+	assertTrue("firstDate not first", questions[0] == firstDate);
+	assertTrue("secondDate not second", questions[1] == secondDate);
+	assertTrue("thirdDate not third", questions[2] == thirdDate);
+	assertTrue("fourthDate not last", questions[-1] == fourthDate);
+
+}
---
+++ AgoraTest/src/com/brogrammers/agora/test/SortByVoteTest.java
@@ -0,0 +1,42 @@
+package com.brogrammers.agora.test;
+
+import junit.framework.TestCase;
+
+public class SortByVoteTest extends TestCase {
+
+	QuestionController controller = QuestionController.getController();
+	WebServiceModel webModel = WebServiceModel.getModel();
+	List<Question> questions = weModel.getQuestions();
+	
+	Question tenVote = new Question("Test post please ignore", "ignore pls", null );
+	Question fiveVote = new Question("Why can't my Meowth talk?", "talk pls", null );
+	Question threeVote = new Question("Where is infinity and beyond?", "the claw", null );
+	Question noVote = new Question("There's a snake in my boot.", "howdy howdy howdy", null );
+	
+	//Order should be {noVote, fiveVote, threeVote, tenVote}
+	long noID = controller.addQuestion(noVote);
+	long fiveID = controller.addQuestion(fiveVote);
+	long threeVote = controller.addQuestion(threeVote);
+	long tenID = controller.addQuestion(tenVote);
+	
+	//upvoting
+	for (int i = 0; i < 10; i++){
+		controller.upvote(tenID);
+	}
+	for (int i = 0; i < 5; i++){
+		controller.upvote(fiveID);
+	}
+	for (int i = 0; i < 3; i++){
+		controller.upvote(threeID);
+	}
+	
+	//assuming enum {VOTES, DATE};
+	questions.sortBy(0);
+	
+	//order should now be { tenVote, fiveVote, threeVote, noVote}
+	assertTrue("tenVote not first", questions[0] == tenVote);
+	assertTrue("fiveVote not second", questions[1] == fiveVote);
+	assertTrue("threeVote not third", questions[2] == threeVote);
+	assertTrue("noVote not last", questions[-1] == noVote);
+
+}
