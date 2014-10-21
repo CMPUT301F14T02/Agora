4a53441376ba40ae5c172aeafbfc20e5ba6b2d32
Kevin Tang <tkevin@ualberta.ca>
2014-10-21 01:22:11 -0600

    Fixed R.java auto generation

--- Agora/gen/com/brogrammers/agora/R.java
+++ Agora/gen/com/brogrammers/agora/R.java
@@ -22,21 +22,54 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_action_flag=0x7f020000;
+        public static final int ic_action_new=0x7f020001;
+        public static final int ic_action_rating_favoritepink=0x7f020002;
+        public static final int ic_action_search=0x7f020003;
+        public static final int ic_action_settings=0x7f020004;
+        public static final int ic_action_upvote=0x7f020005;
+        public static final int ic_action_view_as_list=0x7f020006;
+        public static final int ic_launcher=0x7f020007;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int AuthourDate=0x7f080004;
+        public static final int action_settings=0x7f08000b;
+        public static final int addQuestionBQV=0x7f080008;
+        public static final int imageButton1=0x7f080007;
+        public static final int imageView1=0x7f080006;
+        public static final int listView1=0x7f080000;
+        public static final int qFavourite=0x7f080002;
+        public static final int qScore=0x7f080003;
+        public static final int qTitle=0x7f080005;
+        public static final int qUpvote=0x7f080001;
+        public static final int searchBQV=0x7f080009;
+        public static final int sortBQV=0x7f08000a;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int question_object=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
+        public static final int add=0x7f050004;
+        public static final int addPictureButton=0x7f05000d;
+        public static final int addQuestion=0x7f050003;
         public static final int app_name=0x7f050000;
+        public static final int enterbodytext=0x7f05000c;
+        public static final int entertitletext=0x7f05000b;
         public static final int hello_world=0x7f050001;
+        public static final int search=0x7f050005;
+        public static final int sort=0x7f050007;
+        public static final int sortBy=0x7f050006;
+        public static final int submitAnswerButton=0x7f05000f;
+        public static final int submitQuestionButton=0x7f05000e;
+        public static final int title_activity_author_answer=0x7f050010;
+        public static final int title_activity_author_question=0x7f050008;
+        public static final int titlebodytext=0x7f05000a;
+        public static final int titletext=0x7f050009;
     }
     public static final class style {
         /** 
