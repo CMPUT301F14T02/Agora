5cdb037ca3225aec662f1ae17ee0893f5c02b67f
Dan <djphan@gmail.com>
2014-10-22 09:00:16 -0600

    Altered Main activity for main view

--- Agora/src/com/brogrammers/agora/MainActivity.java
+++ Agora/src/com/brogrammers/agora/MainActivity.java
@@ -1,25 +1,34 @@
 package com.brogrammers.agora;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.PopupMenu;
+import android.widget.SearchView;
 import android.widget.Toast;
-
 
 public class MainActivity extends Activity {
 
+
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+    	// Dan Global Search Manager?
+        SearchManager searchManager = (SearchManager) getSystemService(Agora.getContext().SEARCH_SERVICE);
         return true;
     }
 
@@ -29,13 +38,13 @@ public class MainActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int item_id = item.getItemId();
-         
+        
         switch (item_id) {
         case R.id.addQuestionBQV:
         	openAddQuestionView();
         	return true;
         case R.id.searchBQV:
-        	openSearchDropDown();
+        	openSearchBar(item);
         	return true;
         case R.id.sortBQV:
         	openSortContextMenu();
@@ -47,22 +56,22 @@ public class MainActivity extends Activity {
     }
         
     public void openAddQuestionView() {
-        // 1. Switch views for add a Question
-    	Toast.makeText(this, "Hook up Add a question here", Toast.LENGTH_SHORT).show();
+    	Intent intent = new Intent(Agora.getContext(), AuthorQuestionActivity.class);
+    	startActivity(intent);
+    	//Toast.makeText(Agora.getContext(), "Hook up Add a question here", Toast.LENGTH_SHORT).show();
     }
     
-    public void openSearchDropDown() {
-        // 2. Create a dropdown menu for the search
-    	Toast.makeText(this, "Add Dropdown Search", Toast.LENGTH_SHORT).show();
+    public void openSearchBar(MenuItem item) {
+        SearchView searchView = (SearchView) item.getActionView();
+    	Toast.makeText(Agora.getContext(), "Add Dropdown Search", Toast.LENGTH_SHORT).show();
     }
     
     public void openSortContextMenu() {
-        // 2. Create a dropdown menu for the search
-    	Toast.makeText(this, "Sort Context", Toast.LENGTH_SHORT).show();
+        // 3. Create a context menu for sort 
+    	Toast.makeText(Agora.getContext(), "Sort Context", Toast.LENGTH_SHORT).show();
     }
     
     
 
-    // 3. Create a context menu for sort 
     
 }
