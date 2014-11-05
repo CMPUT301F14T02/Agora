aabb65d3527cf262d820c293f6baf16f02dc72cd
jaeyoon1 <jaeyoon1@ualberta.ca>
2014-11-05 16:58:27 -0700

    Update to CommentAdapter change adapter to reflect the fact that questions can also have comments

--- Agora/src/com/brogrammers/agora/CommentAdapter.java
+++ Agora/src/com/brogrammers/agora/CommentAdapter.java
@@ -13,22 +13,39 @@ import android.widget.TextView;
 public class CommentAdapter extends BaseAdapter {
 	private Answer answer;
 	private LayoutInflater inflater;
-
+	private Question question;
+	
 	CommentAdapter(Answer a){
 		this.answer = a;
+		this.question = null;
 		this.inflater = (LayoutInflater)Agora.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);	
 	}
+	
+	CommentAdapter(Question q){
+		this.answer = null;
+		this.question = null;
+		this.inflater = (LayoutInflater)Agora.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);	
+	}
+	
 
 	@Override
 	public int getCount() {
 		// TODO Auto-generated method stub
-		return answer.countComments();
+		if(question == null){
+			return answer.countComments();
+		} else {
+			return question.countComments();
+		}
 	}
 
 	@Override
 	public Object getItem(int position) {
 		// TODO Auto-generated method stub
-		return answer.getComments().get(position);
+		if(question == null){
+			return answer.getComments().get(position);
+		} else {
+			return answer.getComments().get(position);
+		}
 	}
 
 	@Override
