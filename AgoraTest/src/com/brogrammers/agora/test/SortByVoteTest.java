2a7929262fa4257f35d338434f5050ca222b440f
Tian <wtianzhi@gmail.com>
2014-11-06 16:22:35 -0700

    Made sorting tests, sorting tests pass

--- AgoraTest/src/com/brogrammers/agora/test/SortByVoteTest.java
+++ AgoraTest/src/com/brogrammers/agora/test/SortByVoteTest.java
@@ -1,46 +1,135 @@
-/*package com.brogrammers.agora.test;
+package com.brogrammers.agora.test;
 
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
-import com.brogrammers.agora.ElasticSearch;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpDelete;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.brogrammers.agora.CacheDataManager;
+import com.brogrammers.agora.DeviceUser;
+import com.brogrammers.agora.ESDataManager;
+import com.brogrammers.agora.MainActivity;
 import com.brogrammers.agora.Question;
 import com.brogrammers.agora.QuestionController;
 
 import junit.framework.TestCase;
 
-public class SortByVoteTest extends TestCase {
 
-	QuestionController controller = QuestionController.getController();
-	ElasticSearch webModel = ElasticSearch.getInstance();
-	List<Question> questions = webModel.getQuestions();
+public class SortByVoteTest extends ActivityInstrumentationTestCase2<MainActivity> {
 
-	//Order should be {noVote, fiveVote, threeVote, tenVote}
-	long noID = controller.addQuestion("There's a snake in my boot.", "howdy howdy howdy", null );
-	long fiveID = controller.addQuestion("Why can't my Meowth talk?", "talk pls", null );
-	long threeID = controller.addQuestion("Where is infinity and beyond?", "the claw", null );
-	long tenID = controller.addQuestion("Test post please ignore", "ignore pls", null );
-	
-	//upvoting
-	for (int i = 0; i < 10; i++){
-		controller.upvote(tenID);
+	public SortByVoteTest() {
+		super(MainActivity.class);
 	}
-	for (int i = 0; i < 5; i++){
-		controller.upvote(fiveID);
-	}
-	for (int i = 0; i < 3; i++){
-		controller.upvote(threeID);
+
+	protected void setUp() throws Exception {
+		super.setUp();
+		HttpClient client = new DefaultHttpClient();
+		try {
+			HttpDelete deleteRequest = new HttpDelete("http://cmput301.softwareprocess.es:8080/cmput301f14t02/sorttest/_query?q=_type:sorttest");
+			client.execute(deleteRequest);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
-	//assuming enum {VOTES, DATE};
-	questions.sortBy(0);
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
 	
-	//order should now be { tenVote, fiveVote, threeVote, noVote}
-	void testSortOnVote() {
-		assertTrue("tenVote not first", questions[0] == tenVote);
-		assertTrue("fiveVote not second", questions[1] == fiveVote);
-		assertTrue("threeVote not third", questions[2] == threeVote);
-		assertTrue("noVote not last", questions[-1] == noVote);
-		assertTrue("Correct count", questions.size() = 4);
+	private class TestDeviceUser extends DeviceUser {
+		public TestDeviceUser() {
+			setUsername("TestBingsF");
+			favoritesPrefFileName = "TEST_FAVORITES";
+			cachedPrefFileName = "TEST_CACHED";
+			authoredPrefFileName = "TEST_AUTHORED";
+			usernamePrefFileName = "TEST_USERNAME";
+		}
+	}
+	
+	private class TestCacheManager extends CacheDataManager {
+		public TestCacheManager() {
+			super("TEST_CACHE");
+		}
+	}
+	
+	private class TestESManager extends ESDataManager {
+		public TestESManager() {
+			super("http://cmput301.softwareprocess.es:8080/", "cmput301f14t02/", "sorttest/");
+		}
+	}
+	
+	private class TestController extends QuestionController {
+		public TestController(DeviceUser user, CacheDataManager cache, ESDataManager es) {
+			super(user, cache, es);
+		}
+	}
+	
+	public void testSorting() throws Throwable {
+		final CountDownLatch signal = new CountDownLatch(1);
+		final List<ArrayList<Question>> results = new ArrayList<ArrayList<Question>>();
+
+		DeviceUser user = new TestDeviceUser();
+		CacheDataManager cache = new TestCacheManager();
+		final ESDataManager es = new TestESManager();
+		QuestionController controller = new TestController(user, cache, es);
+
+		controller.addQuestion("Question 1", "ignore pls", null);
+		controller.addQuestion("Question 2", "talk pls", null);
+		controller.addQuestion("Question 3", "the claw", null);
+
+		// wait for it to be uploaded
+		signal.await(2, TimeUnit.SECONDS);
+
+		// check that the question was pushed to the ES server
+		runTestOnUiThread(new Runnable() { public void run() {
+				try {
+					results.add((ArrayList<Question>)es.getQuestions());
+				} catch (UnsupportedEncodingException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		
+		// wait for the response
+		signal.await(2, TimeUnit.SECONDS);
+		
+		List<Question> questions = results.get(0);
+		
+		Long id0 = questions.get(0).getID();
+		Long id1 = questions.get(1).getID();
+		Long id2 = questions.get(2).getID();
+		
+
+		for(int i = 0; i>500; i++) {
+			questions.get(2).upvote();
+		}
+		for(int i = 0; i>100; i++) {
+			questions.get(1).upvote();
+		}
+		for(int i = 0; i>10; i++) {
+			questions.get(0).upvote();
+		}
+
+		Collections.sort(questions, new Comparator<Question>() {
+		    public int compare(Question m1, Question m2) {
+		        return ((Integer) m1.getRating()).compareTo(m2.getRating());
+		    }
+		});
+		
+		//order should now be { firstDate, secondDate, thirdDate, fourthDate}
+		assertTrue("Correct count", questions.size() == 3);
+		assertTrue("500 Votes not first", questions.get(0).getID() == id2);
+		assertTrue("200 Votes not second", questions.get(1).getID().equals(id1));
+		assertTrue("10 Vote not third", questions.get(2).getID() == id0);
 	}
 }
-*/
