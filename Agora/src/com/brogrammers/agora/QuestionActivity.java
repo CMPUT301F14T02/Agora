7addaff8e92c75919b05eca298454e45f41c8656
Carl Sobolewski <sobolews@ualberta.ca>
2014-11-06 14:41:05 -0700

    Fixed padding error in MainActivity

--- Agora/src/com/brogrammers/agora/QuestionActivity.java
+++ Agora/src/com/brogrammers/agora/QuestionActivity.java
@@ -23,7 +23,7 @@ public class QuestionActivity extends Activity implements Observer {
 	private Long qid;
 	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) { 
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_question);
 		
