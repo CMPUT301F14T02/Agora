342df424356f7fc8c96692871d7de89c669b1bb4
Dan <djphan@gmail.com>
2014-11-07 14:10:18 -0700

    Added docs to DataManager. Modified DataManager and Cache DataManager methods from boolean to void.

--- Agora/src/com/brogrammers/agora/DataManager.java
+++ Agora/src/com/brogrammers/agora/DataManager.java
@@ -3,10 +3,47 @@ package com.brogrammers.agora;
 import java.io.UnsupportedEncodingException;
 import java.util.List;
 
+/** Implements the basic data functions required to work on a list of questions
+ *  and add questions, add answers, and add comments to the list of question objects.
+ *  All classes that implements the DataManager all require these set of functionalities.
+ * 
+ * @author Team 02
+ *
+ */
+
 
 public interface DataManager {
+	/**
+	 * This gets the list of all the question objects in our app.
+	 * @return List<Question> 		a list of questions
+	 * @throws UnsupportedEncodingException
+	 */
 	List<Question> getQuestions() throws UnsupportedEncodingException;
-	boolean pushQuestion(Question q) throws UnsupportedEncodingException;
-	boolean pushAnswer(Answer a, Long qID) throws UnsupportedEncodingException;
-	boolean pushComment(Comment c, Long qID, Long aID) throws UnsupportedEncodingException;
+	
+	/**
+	 * This pushes a question object into our question list
+	 * 
+	 * @param q		the question we are pushing
+	 * @throws UnsupportedEncodingException
+	 */
+	void pushQuestion(Question q) throws UnsupportedEncodingException;
+	
+	
+	/**
+	 * Pushes an answer to the target question object that contains the list of answers
+	 * @param a 	the answer object to add to the quesiton
+	 * @param qID	the unique question ID (i.e. the location where the answer is going)
+	 * @throws UnsupportedEncodingException
+	 */
+	void pushAnswer(Answer a, Long qID) throws UnsupportedEncodingException;
+	
+	
+	/**
+	 * Pushes a comment to the target question or answer
+	 * @param c 		the comment object
+	 * @param qID 		the target question ID
+	 * @param aID 		the target answer ID
+	 * @throws UnsupportedEncodingException
+	 */
+	void pushComment(Comment c, Long qID, Long aID) throws UnsupportedEncodingException;
 }
--- Agora/src/com/brogrammers/agora/Observer.java
+++ Agora/src/com/brogrammers/agora/Observer.java
@@ -1,5 +1,15 @@
 package com.brogrammers.agora;
 
+
+/**
+ * Observer interface provides the implementation methods to update
+ * our object that uses observers. E.g. update the controller when it 
+ * fetches data from ES server
+ *  
+ * @author Team 02
+ *
+ */
+
 public interface Observer {
 	public void update(); 
 }
