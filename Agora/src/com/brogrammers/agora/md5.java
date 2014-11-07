e469fd0e78629a1af7015495f74db4e018a25992
Dan <djphan@gmail.com>
2014-11-07 14:19:34 -0700

    Added docs to md5

--- Agora/src/com/brogrammers/agora/md5.java
+++ Agora/src/com/brogrammers/agora/md5.java
@@ -3,7 +3,14 @@ package com.brogrammers.agora;
 import java.nio.ByteBuffer;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-
+/**
+ * We use an md5 hash function to generate the unique ids of the questions, and answers 
+ * to retrieve the correct question/answer. This can be applied to any other data object.
+ *  
+ * More info about md5 hashing can be found here: (http://en.wikipedia.org/wiki/MD5)
+ * @author Team 02
+ *
+ */
 public class md5 {
 	static public Long hash(String s) {
 		try {
