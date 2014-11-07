443d88dccc1a66feef7443bf4972c82cc256c295
Dan <djphan@gmail.com>
2014-11-07 13:28:44 -0700

    added javadocs

--- Agora/src/com/brogrammers/agora/Agora.java
+++ Agora/src/com/brogrammers/agora/Agora.java
@@ -3,6 +3,11 @@ package com.brogrammers.agora;
 import android.app.Application;
 import android.content.Context;
 
+/**
+ * Application level class used to pull the context across different classes.
+ * @author Team 02
+ */
+
 public class Agora extends Application {
 	private static Context context;
 	
