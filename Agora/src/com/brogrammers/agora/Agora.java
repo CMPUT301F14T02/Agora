e487805e91989052e2e4b7bd8cc7fa82e5035605
Carl Sobolewski <sobolews@ualberta.ca>
2014-11-07 13:22:07 -0700

    Javadoc'd the application class

--- Agora/src/com/brogrammers/agora/Agora.java
+++ Agora/src/com/brogrammers/agora/Agora.java
@@ -5,15 +5,20 @@ import android.content.Context;
 
 public class Agora extends Application {
 	private static Context context;
-	
+
+	/**
+	 * The application class stores its own context so that it may be accessed
+	 * globally.
+	 * 
+	 * @return the application context
+	 */
 	public static Context getContext() {
 		return context;
 	}
-	
+
 	@Override
 	public void onCreate() {
 		super.onCreate();
-		
 		context = getApplicationContext();
 	}
 }
